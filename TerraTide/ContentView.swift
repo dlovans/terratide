fbd02bc0974ef71bace8515f64b19130667997bf
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2025-01-21 15:40:56 +0100

    Initial Commit

---
+++ TerraTide/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TerraTide
+//
+//  Created by Dlovan Sharif on 2025-01-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TerraTide/TerraTideApp.swift
@@ -0,0 +1,17 @@
+//
+//  TerraTideApp.swift
+//  TerraTide
+//
+//  Created by Dlovan Sharif on 2025-01-21.
+//
+
+import SwiftUI
+
+@main
+struct TerraTideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
