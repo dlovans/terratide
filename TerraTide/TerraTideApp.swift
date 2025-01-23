9236d3f5da0c62efa240fdb02ba400bc40376ebe
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2025-01-24 00:02:57 +0100

    Lock app in portrait mode

--- TerraTide/TerraTideApp.swift
+++ TerraTide/TerraTideApp.swift
@@ -9,9 +9,18 @@ import SwiftUI
 
 @main
 struct TerraTideApp: App {
+    @UIApplicationDelegateAdaptor var appDelegate: AppDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        .portrait
     }
 }
