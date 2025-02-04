bb4bcabf9673227bcab71632de892df1bcd635bf
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2025-02-04 18:19:08 +0100

    Define empty TidePageView

---
+++ TerraTide/View/TideView/TidePageView.swift
@@ -0,0 +1,19 @@
+//
+//  TidePageView.swift
+//  TerraTide
+//
+//  Created by Dlovan Sharif on 2025-02-02.
+//
+
+import SwiftUI
+
+struct TidePageView: View {
+    let id: Int
+    var body: some View {
+        Text("\(id)")
+    }
+}
+
+#Preview {
+    TidePageView(id: 0)
+}
