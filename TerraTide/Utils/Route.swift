e9c3786dd1131fc644b5f05926e43ec49acd712f
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2025-02-04 18:16:51 +0100

    Define Route enum, representing paths in app

---
+++ TerraTide/Utils/Route.swift
@@ -0,0 +1,13 @@
+//
+//  Route.swift
+//  TerraTide
+//
+//  Created by Dlovan Sharif on 2025-02-04.
+//
+
+import Foundation
+
+enum Route: Hashable {
+    case general(String)
+    case tide(Tide)
+}
