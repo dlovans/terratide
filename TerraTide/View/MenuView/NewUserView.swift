edd61389ab0ba9be8a3fa5f9c5e40040431ad6c3
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2025-02-13 16:12:47 +0100

    Declare NewUserView prompting new users to assign username and date of birth

---
+++ TerraTide/View/MenuView/NewUserView.swift
@@ -0,0 +1,101 @@
+//
+//  NewUserView.swift
+//  TerraTide
+//
+//  Created by Dlovan Sharif on 2025-02-10.
+//
+
+import SwiftUI
+
+struct NewUserView: View {
+    @State private var username: String = ""
+    @State private var usernameFeedback: String = ""
+    @State private var usernameIsValid: Bool = false
+    @State private var birthDate: Date = {
+            var dateComponents = DateComponents()
+            dateComponents.year = 2005
+            dateComponents.month = 1
+            dateComponents.day = 1
+            return Calendar.current.date(from: dateComponents) ?? Date()
+        }()
+    
+    let dateRange: ClosedRange<Date> = {
+        let calendar = Calendar.current
+        let fifteenYearsAgo = calendar.date(byAdding: .year, value: -15, to: Date())!
+        let distantPast = calendar.date(byAdding: .year, value: -50, to: Date())!
+        return distantPast...fifteenYearsAgo
+    }()
+    
+    var body: some View {
+        VStack (spacing: 40) {
+            VStack {
+                Text("Create a username (cannot be changed):")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                HStack {
+                    TextField("", text: $username)
+                }
+                .padding()
+                .overlay {
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(.black, lineWidth: 1)
+                }
+                Text(usernameFeedback)
+                    .font(.footnote)
+                    .foregroundStyle(usernameIsValid ? Color.green : Color.red)
+                    .frame(maxWidth: .infinity, minHeight: 15, alignment: .topLeading)
+            }
+            .onChange(of: username) { oldValue, newValue in
+                withAnimation {
+                    if newValue.isEmpty {
+                        usernameIsValid = false
+                        usernameFeedback = "Username cannot be empty."
+                    } else if newValue.contains(" ") {
+                        usernameIsValid = false
+                        usernameFeedback = "Username cannot contain spaces."
+                    } else if newValue.count > 15 {
+                        usernameIsValid = false
+                        usernameFeedback = "Username cannot be longer than 15 characters."
+                    } else {
+                        usernameFeedback = ""
+                        usernameIsValid = true
+                    }
+                }
+            }
+            
+            VStack {
+                Text("Date of birth (used to display relevant Tides):")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+
+                DatePicker("", selection: $birthDate, in: dateRange, displayedComponents: .date)
+                    .datePickerStyle(WheelDatePickerStyle())
+                    .padding()
+            }
+            
+            Button {
+            // Create
+            } label: {
+                HStack {
+                    Image(systemName: "checkmark")
+                    Text("Create")
+                }
+                .foregroundStyle(.black)
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(!usernameIsValid ? .gray : .orange.opacity(0.7))
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+            }
+            .buttonStyle(RemoveHighlightButtonStyle())
+        }
+        .padding()
+    }
+    
+    private var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        return formatter
+    }
+}
+
+#Preview {
+    NewUserView()
+}
