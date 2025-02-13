f03763e74d887a0acb654d56087ebaa4b77b4838
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2025-02-13 16:14:59 +0100

    Refactor AvailableTideListView, display expiration timer, update UI, add report context menu button

--- TerraTide/View/TideView/AvailableTideListView.swift
+++ TerraTide/View/TideView/AvailableTideListView.swift
@@ -11,9 +11,9 @@ struct AvailableTideListView: View {
     @Binding var path: [Route]
     
     let tides: [Tide] = [
-        Tide(id: 0, title: "Dlovan's Psycho Game", description: "Jag ska irritera dig. Första person som blir arg är en n00b.", creator: "@Dlovan", participants: 9999, maxParticipants: 10000),
-        Tide(id: 1, title: "Chess Masters", description: "Tävling för schackspelare", creator: "@Magnus", participants: 50, maxParticipants: 100),
-        Tide(id: 2, title: "Swift Developers", description: "Diskutera Swift och iOS", creator: "@AppleDev", participants: 120, maxParticipants: 500)
+        Tide(id: "0", title: "Dlovan's Psycho Game", description: "Jag ska irritera dig. Första person som blir arg är en n00b.", creatorId: "Dlovan", participants: 9999, maxParticipants: 10000, joinedUsers: ["Dlovan", "Ibn"]),
+        Tide(id: "1", title: "Chess Masters", description: "Tävling för schackspelare", creatorId: "Magnus", participants: 50, maxParticipants: 100, joinedUsers: ["Dlovan", "Ibn"]),
+        Tide(id: "2", title: "Swift Developers", description: "Diskutera Swift och iOS", creatorId: "AppleDev", participants: 120, maxParticipants: 500, joinedUsers: ["Dlovan", "Ibn"])
     ]
     
     var body: some View {
@@ -37,35 +37,37 @@ struct AvailableTideListView: View {
                         } label: {
                             Image(systemName: "plus.circle")
                                 .foregroundStyle(.orange)
-                                .font(.largeTitle)
+                                .font(.system(size: 28))
                         }
                 }
-                .font(.title2)
                 .padding(.horizontal, 5)
 
                 ScrollView {
                     LazyVStack {
                         ForEach(tides, id: \.self) { tide in
-                            TideItemView(path: $path, tide: tide)
+                            TideItemView(tide: tide, path: $path)
                         }
                     }
                 }
                 .scrollIndicators(.hidden)
             }
         }
+        .padding()
         .frame(maxHeight: .infinity, alignment: .bottom)
     }
 }
 
 struct TideItemView: View {
-    @Binding var path: [Route]
     let tide: Tide
+
+    @Binding var path: [Route]
+    @State private var actionButtonText: String = "Join"
+    @State private var showReportTideSheet: Bool = false
     
     var body: some View {
         ZStack {
             VStack (spacing: 10) {
                 Text(tide.title)
-                    .font(.title3)
                     .frame(maxWidth: .infinity,alignment: .leading)
                 Divider()
                     .background(.gray)
@@ -74,41 +76,58 @@ struct TideItemView: View {
                 Divider()
                     .background(.gray)
                 HStack {
-                    Text(tide.creator)
+                    Text("By: \(tide.creatorId)")
                     Spacer()
                     Image(systemName: "person.fill")
                     Text("\(tide.participants)/\(tide.maxParticipants)")
-                    
                 }
                 HStack {
+                    Text("Expires in:")
+                    Spacer()
+                    Text(Date.now.addingTimeInterval(500), style: .timer)
+                }
+                
+                HStack {
                     Button {
-                        print("clicked")
-                        // Join button opens tide page.
-                        path.append(.tide(tide))
+                        path.append(.tide(String(tide.id)))
                     } label: {
                         HStack {
-                            Text("Join")
+                            Text(actionButtonText)
                             Image(systemName: "arrow.forward")
                         }
                         .frame(maxWidth: .infinity, maxHeight: .infinity)
                         .padding(10)
-                        .background(Color.orange)
+                        .background(.orange)
                         .foregroundStyle(.white)
                         .clipShape(RoundedRectangle(cornerRadius: 5))
                         
                     }
                     .buttonStyle(RemoveHighlightButtonStyle())
                 }
+                
             }
             .padding()
             .overlay {
                 RoundedRectangle(cornerRadius: 10)
                     .stroke(Color.black, lineWidth: 1)
-                    .blur(radius: 5)
+                    .blur(radius: 20)
             }
+            
         }
         .frame(maxWidth: .infinity, alignment: .topLeading)
         .clipShape(RoundedRectangle(cornerRadius: 10))
+        .background(.white)
+        .contextMenu {
+            // If userId != creatorId, display these...
+            Button {
+                showReportTideSheet = true
+            } label: {
+                Text("Report")
+            }
+        }
+        .sheet(isPresented: $showReportTideSheet) {
+            Text("Hello worlnknkd")
+        }
     }
 }
 
