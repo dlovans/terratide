02207d33adc1c89e6b64a0b78732c9b2c045a919
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2025-02-13 19:31:17 +0100

    Declare User model

---
+++ TerraTide/Model/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  TerraTide
+//
+//  Created by Dlovan Sharif on 2025-02-13.
+//
+
+import Foundation
+
+struct User {
+    let id: String
+    let username: String
+    let blockedUserIds: [String]
+    let dateOfBirth: Date?
+}
