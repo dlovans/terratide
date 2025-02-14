c8f2c210279c37f6890933dcca6e34a330ba7e90
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2025-02-14 15:03:10 +0100

    Declare enumeration for authentication status

---
+++ TerraTide/Utils/AuthStatus.swift
@@ -0,0 +1,16 @@
+//
+//  AuthStatus.swift
+//  TerraTide
+//
+//  Created by Dlovan Sharif on 2025-02-13.
+//
+
+import Foundation
+
+enum AuthStatus {
+    case
+        logoutFailure,
+        logoutSuccess,
+        loginFailure,
+        loginSuccess
+}
