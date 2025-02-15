0b4e3915b431c74cfad847ab8d9da19e1fc20332
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2025-02-15 17:06:52 +0100

    Declare a loading view for when app resources are loading

---
+++ TerraTide/View/LoadingView.swift
@@ -0,0 +1,22 @@
+//
+//  LoadingView.swift
+//  TerraTide
+//
+//  Created by Dlovan Sharif on 2025-02-15.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        ZStack {
+            ProgressView()
+                .progressViewStyle(CircularProgressViewStyle())
+                .tint(.orange)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)    }
+}
+
+#Preview {
+    LoadingView()
+}
