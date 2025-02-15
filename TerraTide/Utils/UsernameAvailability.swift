57b82d535d05fe145a5dbb4481acc89bc9aec7f9
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2025-02-15 17:05:51 +0100

    Declare enumuration representing username availability or error of operation

---
+++ TerraTide/Utils/UsernameAvailability.swift
@@ -0,0 +1,12 @@
+//
+//  UsernameAvailability.swift
+//  TerraTide
+//
+//  Created by Dlovan Sharif on 2025-02-15.
+//
+
+import Foundation
+
+enum UsernameAvailability {
+    case available, unavailable, error
+}
