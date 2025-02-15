6e6c141eb8f75c67b7229f1f8fb72a4653dd0f26
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2025-02-15 22:45:27 +0100

    Declare enumeration for status of updating a new user's username and date of birth

---
+++ TerraTide/Utils/UpdateNewUserStatus.swift
@@ -0,0 +1,12 @@
+//
+//  UpdateNewUserStatus.swift
+//  TerraTide
+//
+//  Created by Dlovan Sharif on 2025-02-15.
+//
+
+import Foundation
+
+enum UpdateNewUserStatus {
+    case updateSuccess, updateFailed, unAuthenticatedUser, usernameAlreadyExists
+}
