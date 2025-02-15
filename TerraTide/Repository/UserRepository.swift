2cc28a3d0f0c228fe4c3a45ed4c8c5519b9291cb
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2025-02-15 16:37:48 +0100

    Declare UserRepository for handling user data

---
+++ TerraTide/Repository/UserRepository.swift
@@ -0,0 +1,70 @@
+//
+//  UserRepository.swift
+//  TerraTide
+//
+//  Created by Dlovan Sharif on 2025-02-14.
+//
+
+import Foundation
+import FirebaseFirestore
+import FirebaseAuth
+
+class UserRepository {
+    let db = Firestore.firestore()
+    var userListener: ListenerRegistration? = nil
+    
+    
+    /// Creates a document in the users collection in Firestore.
+    /// - Returns: A boolean to indicate if operation was successful (or if user already exists) or a failure.
+    func createUser() async -> Bool {
+        if let userId = Auth.auth().currentUser?.uid {
+            do {
+                let snapshot = try await db.collection("users").document(userId).getDocument()
+                if !snapshot.exists {
+                    try await db.collection("users").document(userId).setData([
+                        "username": ""
+                    ])
+                }
+                return true
+            } catch {
+                print("Failed to create user document in Firestore: \(error)")
+                return false
+            }
+        } else {
+            return false
+        }
+    }
+    
+    /// Attaches a listener to the user document in Firestore, using uid of currently authenticated user.
+    /// - Parameter completion: A closure that runs when the listener has been attached and when the user document in Firestore updates. Expects an instance of User model or nil.
+    func attachUserListener(completion: @escaping (User?) -> Void) {
+        guard let userData = Auth.auth().currentUser else { return }
+        self.userListener?.remove()
+        self.userListener = nil
+        
+        db.collection("users").document(userData.uid)
+            .addSnapshotListener { documentSnapshot, error in
+                guard let document = documentSnapshot else {
+                    print("Error fetching document: \(error!)")
+                    completion(nil)
+                    return
+                }
+                guard let data = document.data() else {
+                  print("User document data was empty.")
+                    completion(nil)
+                    return
+                }
+                
+                let userObject = User(id: data["id"] as? String ?? "",
+                            username: data["username"] as? String ?? "",
+                            blockedUserIds: data["blockedUserIds"] as? [String] ?? [],
+                            dateOfBirth: data["dateOfBirth"] as? Date ?? Date(),
+                            isBanned: data["isBanned"] as? Bool ?? false,
+                            banReason: data["banReason"] as? String ?? "",
+                            banLiftDate: data["banLiftDate"] as? Date ?? Date()
+                
+                )
+                completion(userObject)
+            }
+    }
+}
