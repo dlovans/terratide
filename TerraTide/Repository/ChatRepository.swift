7a16113dac2f230e43b72c6fd8a86c95a377411e
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2025-02-16 23:22:17 +0100

    Declare ChatRepository with method attaching a geo-based chat listener

---
+++ TerraTide/Repository/ChatRepository.swift
@@ -0,0 +1,59 @@
+//
+//  ChatRepository.swift
+//  TerraTide
+//
+//  Created by Dlovan Sharif on 2025-02-16.
+//
+
+import Foundation
+import FirebaseFirestore
+
+class ChatRepository {
+    let db = Firestore.firestore()
+    
+    /// Fetches geo-based chat messages.
+    /// - Note: Do not confuse with Tide-specific chat messages! Tide-specific chat messages are found in TideRepository.
+    /// - Parameters:
+    ///   - boundingBox: Tuple representing a bounding box, calculated from a user's position.
+    ///   - onUpdate: A closure that runs when fetching messages is completed.
+    /// - Returns: Chat messages listener. Use to reset in ChatViewModel to avoid memory leaks.
+    func attachChatListener(for boundingBox: Coordinate, onUpdate: @escaping ([Message]?) -> Void) -> ListenerRegistration? {
+        let geoChatListener = db.collection("messages")
+            .whereField("longStart", isLessThanOrEqualTo: boundingBox.longitude)
+            .whereField("longEnd", isGreaterThanOrEqualTo: boundingBox.longitude)
+            .whereField("latStart", isLessThanOrEqualTo: boundingBox.latitude)
+            .whereField("latEnd", isGreaterThanOrEqualTo: boundingBox.latitude)
+            .order(by: "timestamp")
+            .limit(to: 50)
+            .addSnapshotListener { querySnapshot, error in
+                if let error {
+                    print("An error occurred while listening for geo chats: \(error)")
+                    onUpdate(nil)
+                }
+                
+                guard let snapshot = querySnapshot else {
+                    print("Error fetching messages for geo chat: \(error!)")
+                    onUpdate(nil)
+                    return
+                }
+                
+                if snapshot.isEmpty {
+                    onUpdate([])
+                    return
+                }
+                
+                let geoMessages: [Message] = snapshot.documents.compactMap { message in
+                    do {
+                        return try message.data(as: Message.self)
+                    } catch {
+                        print("Error decoding message data: \(error)")
+                        return nil
+                    }
+                }
+                
+                onUpdate(geoMessages)
+            }
+        
+        return geoChatListener
+    }
+}
