ff8a2776d4c2632d7ed718a9d7a14a392bfb07ba
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2025-02-16 23:21:12 +0100

    Define model representing a user's location with longitude and latitude values

---
+++ TerraTide/Model/Coordinate.swift
@@ -0,0 +1,13 @@
+//
+//  Coordinate.swift
+//  TerraTide
+//
+//  Created by Dlovan Sharif on 2025-02-16.
+//
+
+import Foundation
+
+struct Coordinate: Equatable {
+    let latitude: Double
+    let longitude: Double
+}
