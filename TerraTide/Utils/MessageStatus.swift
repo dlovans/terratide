3d9f6cf366f2f2bf79f1427596bee846bdf9b0b2
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2025-02-17 22:42:20 +0100

    Declare enumeration for message creation status

---
+++ TerraTide/Utils/MessageStatus.swift
@@ -0,0 +1,12 @@
+//
+//  MessageStatus.swift
+//  TerraTide
+//
+//  Created by Dlovan Sharif on 2025-02-17.
+//
+
+import Foundation
+
+enum MessageStatus {
+    case sent, failedToCreate, emptyMessage, invalidLocation
+}
