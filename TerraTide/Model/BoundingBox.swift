1e0a629d47c5540e10a2d21dffffe70f9e7264a8
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2025-02-17 22:34:26 +0100

    Refactor BoundingBox type to a model

---
+++ TerraTide/Model/BoundingBox.swift
@@ -0,0 +1,15 @@
+//
+//  BoundingBox.swift
+//  TerraTide
+//
+//  Created by Dlovan Sharif on 2025-02-17.
+//
+
+import Foundation
+
+struct BoundingBox {
+    let longStart: Double
+    let longEnd: Double
+    let latStart: Double
+    let latEnd: Double
+}
