c27a954ba5cba5271d93cf2466357909c7a3a180
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2025-02-18 17:01:19 +0100

    Declare TideViewModel with a method calling createTide in TideRepository

--- TerraTide/ViewModel/TideViewModel.swift
+++ TerraTide/ViewModel/TideViewModel.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+class TideViewModel: ObservableObject {
+    let tideRepository = TideRepository()
+    
+    /// Calls method in TideRepository that creates a Tide in Firestore.
+    /// - Parameters:
+    ///   - byUserID: UserID of Tide creator.
+    ///   - byUsername: Username of Tide creator.
+    ///   - tideTitle: Title of Tide.
+    ///   - tideDescription: Tide description.
+    ///   - tideGroupSize: Max number of participants of Tide.
+    /// - Returns: Tide creation status. Refer to `TideCreationStatus` in `Utils` directory.
+    func createTide(
+        byUserID: String,
+        byUsername: String,
+        tideTitle: String,
+        tideDescription: String,
+        tideGroupSize: Int
+    ) async -> TideCreationStatus {
+        return await tideRepository.createTide(byUserID: byUserID, byUsername: byUsername, tideTitle: tideTitle, tideDescription: tideDescription, tideGroupSize: tideGroupSize)
+    }
+}
