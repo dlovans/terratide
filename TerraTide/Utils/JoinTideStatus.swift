6754df19b712736604635507f9748572c0a96077
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2025-02-20 17:53:21 +0100

    Declare enumeration representing status of Tide join attempt

---
+++ TerraTide/Utils/JoinTideStatus.swift
@@ -0,0 +1,12 @@
+//
+//  JoinTideStatus.swift
+//  TerraTide
+//
+//  Created by Dlovan Sharif on 2025-02-20.
+//
+
+import Foundation
+
+enum JoinTideStatus {
+    case joined, failed, full, invalidTide, noDocument, alreadyJoined
+}
