f6aba8a5300047f1db276bfc67a296cd4760c832
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2025-02-21 22:12:55 +0100

    Define an enumeration representing statuses of leaving a Tide

---
+++ TerraTide/Utils/LeaveTideStatus.swift
@@ -0,0 +1,12 @@
+//
+//  LeaveTideStatus.swift
+//  TerraTide
+//
+//  Created by Dlovan Sharif on 2025-02-21.
+//
+
+import Foundation
+
+enum LeaveTideStatus {
+    case left, invalidData, failed, noDocument, notMember
+}
