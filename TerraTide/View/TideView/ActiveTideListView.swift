b7d8fb180a1f181305315f199ba6063bd51c8603
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2025-02-21 01:07:03 +0100

    Refactor ActiveTideListView to dynamically show Tides data

--- TerraTide/View/TideView/ActiveTideListView.swift
+++ TerraTide/View/TideView/ActiveTideListView.swift
@@ -1,5 +1,5 @@
 //
-//  ActiveTidesView.swift
+//  ActiveTideListView.swift
 //  TerraTide
 //
 //  Created by Dlovan Sharif on 2025-02-02.
@@ -10,95 +10,83 @@ import SwiftUI
 struct ActiveTideListView: View {
     @Binding var path: [Route]
     
+    @EnvironmentObject private var tidesViewModel: TidesViewModel
+    @EnvironmentObject private var userViewModel: UserViewModel
+    
     let tides: [Tide] = [
-        Tide(id: "0", title: "Dlovan's Psycho Game", description: "Jag ska irritera dig. Första person som blir arg är en n00b.", creatorId: "Dlovan", creatorUsername: "dsf", participantCount: 123, maxParticipants: 10000, memberIds: ["Dlovan":"dlo", "Ibn":"asr"], expiryDate: Date()),
-        Tide(id: "1", title: "Chess Masters", description: "Tävling för schackspelare", creatorId: "Magnus", creatorUsername: "asds", participantCount: 123, maxParticipants: 100, memberIds: ["Dlovan":"dlo", "Ibn":"asr"], expiryDate: Date()),
-        Tide(id: "2", title: "Swift Developers", description: "Diskutera Swift och iOS", creatorId: "AppleDev", creatorUsername: "asdsad", participantCount: 123, maxParticipants: 500, memberIds: ["Dlovan":"dlo", "Ibn":"asr"], expiryDate: Date())
     ]
     
     var body: some View {
         ZStack {
-            VStack {
-                HStack {
-                    Text("My Tides")
-                }
-                ScrollView {
-                    LazyVStack {
-                        ForEach(tides, id: \.self) { tide in
-                            ActiveTideItemView(
-                                id: tide.id!,
-                                creatorId: tide.creatorId,
-                                title: tide.title,
-                                description: tide.description,
-                                participants: tide.memberIds.count,
-                                maxParticipants: tide.maxParticipants,
-                                path: $path
-                            )
+            if tidesViewModel.activeTidesHaveLoaded {
+                VStack {
+                    HStack {
+                        Text("My Tides")
+                    }
+                    ScrollView {
+                        LazyVStack {
+                            ForEach(tidesViewModel.activeTides, id: \.self) { tide in
+                                ActiveTideItemView(
+                                    tide: tide,
+                                    path: $path
+                                )
+                            }
                         }
                     }
+                    .scrollIndicators(.hidden)
+                    .padding(.top, 10)
                 }
-                .scrollIndicators(.hidden)
-                .padding(.top, 10)
+            } else {
+                LoadingView()
             }
         }
         .frame(maxHeight: .infinity, alignment: .top)
         .padding(.top, 7)
         .padding()
+        .onAppear {
+            Task { @MainActor in
+                if let userId = userViewModel.user?.id {
+                    tidesViewModel.attachActiveTidesListener(userId: userId)
+                }
+            }
+        }
+        .onDisappear {
+            Task { @MainActor in
+                tidesViewModel.removeActiveTidesListener()
+            }
+        }
     }
 }
 
 // Joined and created tides, sorted by time, descending order.
 struct ActiveTideItemView: View {
-    let id: String
-    let creatorId: String
-    let title: String
-    let description: String
-    let participants: Int
-    let maxParticipants: Int
-    let now = Date()
+    let tide: Tide
 
     @Binding var path: [Route]
+    
+    @EnvironmentObject private var userViewModel: UserViewModel
     @State private var showReportTideSheet: Bool = false
     
     var body: some View {
         ZStack {
             VStack (spacing: 10) {
-                Text(title)
+                Text(tide.title)
                     .frame(maxWidth: .infinity,alignment: .leading)
                 Divider()
                     .background(.gray)
-                Text(description)
+                Text(tide.description)
                     .frame(maxWidth: .infinity,alignment: .leading)
                 Divider()
                     .background(.gray)
+                
                 HStack {
-                    Text("Joined: ")
-                    Spacer()
-                    Image(systemName: "clock.fill")
-                        .foregroundStyle(.orange)
-                    Text(now, style: .time)
-                        .font(.system(size: 12))
-                    Text(now, style: .date)
-                        .font(.system(size: 12))
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                HStack {
-                    Text("By: \(creatorId)")
+                    Text("By: \(tide.creatorUsername)")
                     Spacer()
                     Image(systemName: "person.fill")
-                    Text("\(participants)/\(maxParticipants)")
+                    Text("\(tide.participantCount)/\(tide.maxParticipants)")
                         .font(.system(size: 14))
                 }
-                HStack {
-                    Text(now.addingTimeInterval(7200) < Date() ? "Closed" : "Closing: ")
-                    Spacer()
-                    Image(systemName: "clock.fill")
-                        .foregroundStyle(now.addingTimeInterval(7200) < Date() ? .red : .orange)
-                    Text(now.addingTimeInterval(7200), style: .time)
-                        .font(.system(size: 12))
-                    Text(now.addingTimeInterval(7200), style: .date)
-                        .font(.system(size: 12))
-                }
+                
                 HStack {
                     Button {
                         // Leave button, deactivating tide for this user.
@@ -118,8 +106,7 @@ struct ActiveTideItemView: View {
                     }
                     .buttonStyle(RemoveHighlightButtonStyle())
                     Button {
-                        // Join button opens tide page.
-                        path.append(.tide(String(id)))
+                        path.append(.tide(tide.id!))
                         
                     } label: {
                         HStack {
@@ -147,15 +134,16 @@ struct ActiveTideItemView: View {
         .clipShape(RoundedRectangle(cornerRadius: 10))
         .background(.white)
         .contextMenu {
-            // If userId != creatorId, display these...
-            Button {
-                showReportTideSheet = true
-            } label: {
-                Text("Report")
+            if userViewModel.user?.id != tide.creatorId {
+                Button {
+                    showReportTideSheet = true
+                } label: {
+                    Text("Report")
+                }
             }
         }
         .sheet(isPresented: $showReportTideSheet) {
-            Text("Report Tide")
+            Text(tide.id!)
         }
     }
 }
