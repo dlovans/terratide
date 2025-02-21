22e3ee7791dd62bac8d2a0e4b4d3d63de8fbcfa7
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2025-02-21 22:14:02 +0100

    Refactor ActiveTideListView to handle leaving a Tide

--- TerraTide/View/TideView/ActiveTideListView.swift
+++ TerraTide/View/TideView/ActiveTideListView.swift
@@ -12,6 +12,7 @@ struct ActiveTideListView: View {
     
     @EnvironmentObject private var tidesViewModel: TidesViewModel
     @EnvironmentObject private var userViewModel: UserViewModel
+    @State private var openingOrLeavingTide: Bool = false
     
     let tides: [Tide] = [
     ]
@@ -28,7 +29,8 @@ struct ActiveTideListView: View {
                             ForEach(tidesViewModel.activeTides, id: \.self) { tide in
                                 ActiveTideItemView(
                                     tide: tide,
-                                    path: $path
+                                    path: $path,
+                                    openingOrLeavingTide: $openingOrLeavingTide
                                 )
                             }
                         }
@@ -44,6 +46,7 @@ struct ActiveTideListView: View {
         .padding(.top, 7)
         .padding()
         .onAppear {
+            openingOrLeavingTide = false
             Task { @MainActor in
                 if let userId = userViewModel.user?.id {
                     tidesViewModel.attachActiveTidesListener(userId: userId)
@@ -63,9 +66,13 @@ struct ActiveTideItemView: View {
     let tide: Tide
 
     @Binding var path: [Route]
+    @Binding var openingOrLeavingTide: Bool
     
     @EnvironmentObject private var userViewModel: UserViewModel
+    @EnvironmentObject private var singleTideViewModel: SingleTideViewModel
     @State private var showReportTideSheet: Bool = false
+    @State private var errorMessage: String = ""
+    @State private var displayErrorMessage: Bool = false
     
     var body: some View {
         ZStack {
@@ -89,8 +96,38 @@ struct ActiveTideItemView: View {
                 
                 HStack {
                     Button {
-                        // Leave button, deactivating tide for this user.
-                        // Left this tide...
+                        Task { @MainActor in
+                            openingOrLeavingTide = true
+                            let status  = await singleTideViewModel.leaveTide(tideId: tide.id ?? "", userId: userViewModel.user?.id ?? "")
+                            
+                            var isError: Bool = false
+                            
+                            switch status {
+                            case .left:
+                                print("Successfully left Tide.")
+                            case .invalidData:
+                                errorMessage = "Failed to leave :("
+                                isError = true
+                            case .noDocument:
+                                errorMessage = "Couldn't find Tide :("
+                                isError = true
+                            case .notMember:
+                                errorMessage = "You're not a member of this Tide"
+                                isError = true
+                            case .failed:
+                                errorMessage = "Failed to leave, something went wrong. :("
+                                isError = true
+                            }
+                
+                            if isError {
+                                displayErrorMessage = true
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                                    displayErrorMessage = false
+                                }
+                            }
+                            
+                            openingOrLeavingTide = false
+                        }
                     } label: {
                         HStack {
                             Image(systemName: "arrow.backward")
@@ -99,15 +136,15 @@ struct ActiveTideItemView: View {
                         }
                         .frame(maxWidth: .infinity)
                         .padding(10)
-                        .background(Color.red.opacity(0.7))
+                        .background(openingOrLeavingTide ? .gray : Color.red.opacity(0.7))
                         .foregroundStyle(.white)
                         .clipShape(RoundedRectangle(cornerRadius: 5))
+                        .disabled(openingOrLeavingTide)
                         
                     }
                     .buttonStyle(RemoveHighlightButtonStyle())
                     Button {
                         path.append(.tide(tide.id!))
-                        
                     } label: {
                         HStack {
                             Text("Open")
@@ -115,12 +152,13 @@ struct ActiveTideItemView: View {
                         }
                         .frame(maxWidth: .infinity)
                         .padding(10)
-                        .background(Color.orange)
+                        .background(openingOrLeavingTide ? .gray : .orange)
                         .foregroundStyle(.white)
                         .clipShape(RoundedRectangle(cornerRadius: 5))
                         
                     }
                     .buttonStyle(RemoveHighlightButtonStyle())
+                    .disabled(openingOrLeavingTide)
                 }
             }
             .padding()
@@ -128,6 +166,16 @@ struct ActiveTideItemView: View {
                 RoundedRectangle(cornerRadius: 10)
                     .stroke(Color.black, lineWidth: 1)
                     .blur(radius: 20)
+            }
+            .overlay {
+                VStack {
+                    Text(errorMessage)
+                        .foregroundStyle(.white)
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(.black)
+                .scaleEffect(displayErrorMessage ? 1 : 0)
+                .animation(.easeInOut, value: displayErrorMessage)
             }
         }
         .frame(maxWidth: .infinity, alignment: .topLeading)
