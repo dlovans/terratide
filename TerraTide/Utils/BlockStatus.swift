4bdf55b1d4c89534ab6e8fb09a27b8301fc1992c
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2025-02-23 00:04:22 +0100

    Define enum BlockStatus representing the status of blocking a user

---
+++ TerraTide/Utils/BlockStatus.swift
@@ -0,0 +1,12 @@
+//
+//  BlockStatus.swift
+//  TerraTide
+//
+//  Created by Dlovan Sharif on 2025-02-22.
+//
+
+import Foundation
+
+enum BlockStatus {
+    case blocked, missingData, failed, alreadyBlocked, userBlockingNotFound, userToBlockNotFound
+}
