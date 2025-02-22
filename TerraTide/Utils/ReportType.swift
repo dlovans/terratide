1ef7048392039ae01e895418c8c203954b3df7bd
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2025-02-23 00:07:17 +0100

    Define ReportType enum representing the type of report

---
+++ TerraTide/Utils/ReportType.swift
@@ -0,0 +1,15 @@
+//
+//  ReportType.swift
+//  TerraTide
+//
+//  Created by Dlovan Sharif on 2025-02-22.
+//
+
+import Foundation
+
+enum ReportType: String {
+    case
+    tideMessage = "tideMessageReports",
+    geoMessage = "geoMessageReports",
+    tide = "tideReports"
+}
