65b56d84af8c57b4dde59f8b282493a68e632afa
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2025-02-23 00:07:00 +0100

    Define enum ReportStatus representing status of reporting a Tide or message

---
+++ TerraTide/Utils/ReportStatus.swift
@@ -0,0 +1,12 @@
+//
+//  ReportStatus.swift
+//  TerraTide
+//
+//  Created by Dlovan Sharif on 2025-02-22.
+//
+
+import Foundation
+
+enum ReportStatus {
+    case success, failure, missingData
+}
